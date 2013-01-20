fbb297635d0457113b85f43223ac88f406c522ab
Andrzej Zaborowski <balrogg@gmail.com>
2013-01-20 13:54:01 +0100

    Add default tags for attrib layers

--- convert.py
+++ convert.py
@@ -168,80 +168,97 @@ layers = {
         "name": "Funkcja, numer najwyzszej kondygnacji, " + \
             "budynku nieognioodpornego",
         "use": 1,
+        "default": { "building:fireproof": "no" },
     },
     "EBUOA_T_1": {
         "type": "attrib",
         "name": "Funkcja, numer najwyzszej kondygnacji, budynku ognioodpornego",
         "use": 1,
+        "default": { "building:fireproof": "yes" },
     },
     "EBUOA_T_2": {
         "type": "attrib",
         "name": "Opis wieży ognioodpornej będącej budynkiem",
         "use": 1,
+        "default": { "building": "tower", "building:fireproof": "yes" },
     },
     "EBUOA_T_3": {
         "type": "attrib",
         "name": "Etykieta 'wtr' wiatraku ognioodpornego będącego budynkiem",
         "use": 1,
+        "default": { "building": "windmill", "building:fireproof": "yes" },
     },
     "EBUOA_T_4": {
         "type": "attrib",
         "name": "Etykieta 'wtr' wiatraku nieognioodpornego będącego budynkiem",
         "use": 1,
+        "default": { "building": "windmill", "building:fireproof": "no" },
     },
     "EBUOA_T_5": {
         "type": "attrib",
         "name": "Etykieta 'ciepl.' cieplarni, szklarni",
         "use": 1,
+        "default": { "building": "greenhouse" },
     },
     "EBUOA_T_6": {
         "type": "attrib",
         "name": "Etykieta 'r.' bydunku w ruinie",
         "use": 1,
+        "default": { "building": "ruin", "historic": "ruins" },
     },
     "EBTOO_T_0": {
         "type": "attrib",
         "name": "Liczba kondygnacji budynku ognioodpornego",
         "use": 1,
+        "default": { "building:fireproof": "yes" },
     },
     "EBTOO_T_1": {
         "type": "attrib",
         "name": "Liczba kondygnacji budynku nieognioodpornego",
         "use": 1,
+        "default": { "building:fireproof": "no" },
     },
     "EBTOO_T_2": {
         "type": "attrib",
         "name": "Opis wieży ognioodpornej, etykieta wiatraku ognioodpornego",
         "use": 1,
+        "default": { "building:fireproof": "yes" },
     },
     "EBTOO_T_3": {
         "type": "attrib",
         "name": "Etykieta 'wtr' wiatraku nieognioodpornego " + \
                 "nie będącego budynkiem",
         "use": 1,
+        "default": { "building": "windmill", "building:fireproof": "no" },
     },
     "EBTOO_T_4": {
         "type": "attrib",
         "name": "Liczba kondygnacji łącznika napowietrznego budynków",
         "use": 1,
+        "default": { "building": "skyway" },
     },
     "EBTOO_T_5": {
         "type": "attrib",
         "name": "Etykieta 'rmp.' rampy",
         "use": 1,
+        "default": { "building": "ramp" },
     },
     "EBTOO_T_6": {
         "type": "attrib",
         "name": "Etykieta 'f.' fundamentu",
         "use": 1,
+        "default": { "building": "foundation", "building:levels": "0" },
     },
     "EBTOO_T_7": {
         "type": "attrib",
         "name": "Strzałka kierunku wjazdu do podziemia",
+        "use": 0,
     },
     "EBTOO_T_8": {
         "type": "attrib",
         "name": "Komin - symbol",
+        "use": 1,
+        "default": { "building": "chimney", "building:use:pl": "Komin" },
     },
     "EBUPP_L_0": {
         "type": "spatial",
